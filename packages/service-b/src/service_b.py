decccad367f2e6ed7ba8281282a6745619bc92a4
Maxim Gorbatin <mgorbatin@griddynamics.com>
2025-12-17 18:03:52 +0200

    test2

--- packages/service-b/src/service_b.py
+++ packages/service-b/src/service_b.py
@@ -1,1 +1,17 @@
 """Service B package."""
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+x=3
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+async def swagger_ui_redirect(req: Request) -> HTMLResponse:
+    return HTMLResponse()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+async def swagger_ui_redirect1(req: Request) -> HTMLResponse:
+    return HTMLResponse()
